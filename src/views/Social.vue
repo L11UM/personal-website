bd971158174abf32b63db8d2db881700d1e90ac4
LiiUM <liambt20@gmail.com>
2022-06-09 01:04:43 -0700

    Update Social.vue

--- src/views/Social.vue
+++ src/views/Social.vue
@@ -1,6 +1,6 @@
 <template>
-  
+
+<a href=""
 <img src="https://img.shields.io/badge/dynamic/json?logo=twitter&label=Twitter&labelColor=282c34&suffix=+followers&color=1da1f2&query=%24.data.totalSubs&url=https%3A%2F%2Fapi.spencerwoo.com%2Fsubstats%2F%3Fsource%3Dtwitter%26queryKey%3DL3AWM&longCache=true"></img>
 
 </template>
-<a href="https://img.shields.io/badge/dynamic/json?logo=twitter&label=Twitter&labelColor=282c34&suffix=+followers&color=1da1f2&query=%24.data.totalSubs&url=https%3A%2F%2Fapi.spencerwoo.com%2Fsubstats%2F%3Fsource%3Dtwitter%26queryKey%3DL3AWM&longCache=true"></a>
