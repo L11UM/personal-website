7463673144758fee4075da68926649189ff5afa0
LiiUM <liambt20@gmail.com>
2022-12-19 15:15:23 -0700

    ReUp

--- README.md
+++
@@ -1,4 +0,0 @@
- <b>My Personal Site</b>:
-<i>Built with Vue.js. Deployed on Vercel with APIs hosted on Cloudflare</i>
-
-2022
--- src/views/Social.vue
+++
@@ -1,6 +0,0 @@
-<template>
-
-<a href="https://twitter.com/L3AWM">
-<img src="https://img.shields.io/badge/dynamic/json?logo=twitter&label=Twitter&labelColor=282c34&suffix=+followers&color=1da1f2&query=%24.data.totalSubs&url=https%3A%2F%2Fapi.spencerwoo.com%2Fsubstats%2F%3Fsource%3Dtwitter%26queryKey%3DL3AWM&longCache=true"></img>
-</a>
-</template>
